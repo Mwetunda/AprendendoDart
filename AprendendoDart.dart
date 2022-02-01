06e7ca1afdb145bb26893d6f7b991816f83a2f02
Mwetunda <manuelmwetundamw@gmail.com>
2022-02-01 17:12:09 +0100

    Innitial commit 2

--- AprendendoDart.dart
+++ AprendendoDart.dart
@@ -1,13 +1,18 @@
-main()
-{
-  var idade = 20;
+import 'dart:io';
 
-  if(idade >= 18)
-  {
-    print("É maior de idade");
-  }
-  else
-  {
-    print("Não é maior de idade");
+main() {
+  print("Digite a sua idade:");
+  var input = stdin.readLineSync();
+
+  if (input != null) {
+    var idade = int.parse(input);
+
+    if (idade >= 18) {
+      print("É maior de idade");
+    } else {
+      print("Não é maior de idade");
+    }
+  } else {
+    print("Valor null");
   }
 }
